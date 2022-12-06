6ead7ca3fa6f4187765355e3bd9ac51527dee9c3
tonybbb2 <89655535+tonybbb2@users.noreply.github.com>
2022-12-06 02:03:13 -0500

    fix navbar

--- portfolio/pages/components/Navbar.jsx
+++ portfolio/pages/components/Navbar.jsx
@@ -1,51 +1,63 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import Image from 'next/image'
 import { AiOutlineClose, AiOutlineMail } from 'react-icons/ai';
 import { AiOutlineMenu } from 'react-icons/ai';
 import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 
 export const Navbar = () => {
     const [nav, setNav] = useState(false)
     const [shadow, setShadsow] = useState(false)
+    const [navBg, setNavBg] = useState('#ecf0f3');
+    const router = useRouter();
 
     const handleNav = () => {
         setNav(!nav);
     }
 
     useEffect(() => {
+        if (router.asPath === '/') {
+            setNavBg('#ecf0f3')
+        } else {
+            setNavBg('#transparent')
+        }
+    }, [router])
+
+
+    useEffect(() => {
         const handleShadow = () => {
-            if(window.scrollY > 70) {
+            if (window.scrollY > 70) {
                 setShadsow(true)
             } else {
                 setShadsow(false)
             }
         }
         window.addEventListener('scroll', handleShadow)
-    }  , [])
+    }, [])
 
     return (
-        <div className={shadow ? 'fixed w-full h-20 shadow-xl z-[100]' : 'fixed w-full h-20 z-[100]'}>
+        <div style={{ backgroundColor: `${navBg}` }} className={shadow ? 'fixed w-full h-20 shadow-xl z-[100]' : 'fixed w-full h-20 z-[100]'}>
             <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
                 <Image src='/../../portfolio/public/IMG_3544.png' alt='/' width='100' height='80' />
                 <div>
                     <ul className='hidden md:flex'>
                         <Link href='/#main'>
-                        <li className='ml-10 text-sm uppercase hover:border-b'>Home</li>
+                            <li className='ml-10 text-sm uppercase hover:border-b'>Home</li>
                         </Link>
                         <Link href='/#about'>
-                        <li className='ml-10 text-sm uppercase hover:border-b'>About</li>
+                            <li className='ml-10 text-sm uppercase hover:border-b'>About</li>
                         </Link>
                         <Link href='/#skills'>
-                        <li className='ml-10 text-sm uppercase hover:border-b'>Skills</li>
+                            <li className='ml-10 text-sm uppercase hover:border-b'>Skills</li>
                         </Link>
                         <Link href='/#projects'>
-                        <li className='ml-10 text-sm uppercase hover:border-b'>Projects</li>
+                            <li className='ml-10 text-sm uppercase hover:border-b'>Projects</li>
                         </Link>
                         <Link href='/#contact'>
-                        <li className='ml-10 text-sm uppercase hover:border-b'>Contact</li>
+                            <li className='ml-10 text-sm uppercase hover:border-b'>Contact</li>
                         </Link>
                     </ul>
                 </div>
@@ -55,8 +67,8 @@ export const Navbar = () => {
             </div>
             <div className={nav ? 'md:hidden fixed left-0 top-0 w-full h-screen bg-black/80' : ''}>
                 <div className={nav ? ' fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500' :
-                                 'fixed left-[-100%] top-0 p-10 ease-in duration-500'
-                                 }>
+                    'fixed left-[-100%] top-0 p-10 ease-in duration-500'
+                }>
                     <div>
                         <div className='flex w-full items-center justify-between'>
                             <Image src='/../../portfolio/public/IMG_3544.png' alt='/' width='90' height='75' />
@@ -80,18 +92,18 @@ export const Navbar = () => {
                             <p className='uppercase tracking-widest text-red-500'>Let's connect</p>
                             <div className='flex items-center justify-between my-4 w-full sm:w[80%]'>
                                 <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-200'>
-                                    <FaLinkedinIn/>
+                                    <FaLinkedinIn />
                                 </div>
                                 <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-200'>
-                                    <FaGithub/>
+                                    <FaGithub />
                                 </div>
                                 <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-200'>
-                                    <AiOutlineMail/>
+                                    <AiOutlineMail />
                                 </div>
                                 <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-200'>
-                                    <BsFillPersonLinesFill/>
+                                    <BsFillPersonLinesFill />
                                 </div>
-                                
+
                             </div>
                         </div>
                     </div>
