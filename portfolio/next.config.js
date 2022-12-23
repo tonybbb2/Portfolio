d63676f576f83591b6edd0c3c9d6826bc58b6af3
tonybbb2 <89655535+tonybbb2@users.noreply.github.com>
2022-12-23 14:36:46 -0500

    deploy2

--- portfolio/next.config.js
+++ portfolio/next.config.js
@@ -1,6 +1,24 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  env: {
+    API_URL: process.env.NEXT_PUBLIC_API_URL
+  },
+  images: {
+    disableStaticImages: true
+  },
+  webpack: (cfg) => {
+      cfg.module.rules.push(
+          {
+              test: /\.md$/,
+              loader: 'frontmatter-markdown-loader',
+              // options: { mode: ['react-component']}
+          }
+      )
+      return cfg;
+  },
+  target: 'serverless', 
+  eslint: {
+    // Warning: Dangerously allow production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
-
-module.exports = nextConfig
