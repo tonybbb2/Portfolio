1aca322e665e1fbcdaa11e3c43cee53b024a93a6
tonybbb2 <89655535+tonybbb2@users.noreply.github.com>
2024-02-19 01:38:22 -0500

    Progress - retrieving date from markdown file

--- portfolio/pages/blog.jsx
+++ portfolio/pages/blog.jsx
@@ -4,22 +4,43 @@ import matter from 'gray-matter'
 import path from 'path'
 import {NextLink, Link} from 'next/link'
 import { postFilePaths, POSTS_PATH } from './lib/mdxUtils'
+import Article from './components/Article'
 
 
 
 export default function blog({ posts }) {
     console.log(posts);
     return (
-        <div>
-            <h1>Blog</h1>
-            {/* <div className='w-full'>
-                {posts.map((post) => (
-                    <Link key={post.filepath} href={`/blog/${post.filepath.replace(/\.mdx?$/, '')}`}>
-                        {post.data.title}
-                    </Link>
-                ))}
-            </div> */}
+        <div id="Blog">
+        <div className="sm:px-8 mt-4 md:mt-6">
+          <div className="mx-auto w-full max-w-7xl lg:px-8">
+            <div className="relative px-4 sm:px-8 lg:px-12">
+              <div className="mx-auto max-w-2xl lg:max-w-5xl">
+                <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
+                  <div className="flex flex-col gap-16">
+                    <h1 className="text-4xl font-display text-black dark:text-white uppercase ">
+                      <div className="order-last mt-[calc(theme(spacing.16)-theme(spacing.3))]"></div>
+                      <span className="text-xs text-[#DA3028]">
+                        Technology
+                      </span>
+                      <br />
+                        Blogs
+                    </h1>
+                    {posts.map((post, index) => (
+                      <Article
+                        key={index}
+                        time={post.data.time}
+                        title={post.data.title}
+                        description={post.data.description}
+                      />
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
     );
 }
 
