d2d8abca95f82304c91d41aac55d3f9aaa0dfb12
tonybbb2 <89655535+tonybbb2@users.noreply.github.com>
2024-02-23 00:42:46 -0500

    Progress - blogCards component

--- portfolio/pages/blog.jsx
+++ portfolio/pages/blog.jsx
@@ -4,37 +4,36 @@ import matter from 'gray-matter'
 import path from 'path'
 import {NextLink, Link} from 'next/link'
 import { postFilePaths, POSTS_PATH } from './lib/mdxUtils'
-import Article from './components/Article'
+import Image from 'next/image'
+import BlogCards from "./components/BlogCards"
 
-
-
-export default function blog({ posts }) {
-    console.log(posts);
+export default function Blog({ posts }) {
     return (
         <div id="Blog">
-        <div className="sm:px-8 mt-4 md:mt-6">
+        <div className="sm:px-8 mt-16 sm:mt-32">
           <div className="mx-auto w-full max-w-7xl lg:px-8">
             <div className="relative px-4 sm:px-8 lg:px-12">
               <div className="mx-auto max-w-2xl lg:max-w-5xl">
-                <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
-                  <div className="flex flex-col gap-16">
-                    <h1 className="text-4xl font-display text-black dark:text-white uppercase ">
-                      <div className="order-last mt-[calc(theme(spacing.16)-theme(spacing.3))]"></div>
-                      <span className="text-xs text-[#DA3028]">
-                        Technology
-                      </span>
-                      <br />
-                        Blogs
-                    </h1>
-                    {posts.map((post, index) => (
-                      <Article
-                        key={index}
-                        time={post.data.time}
-                        title={post.data.title}
-                        description={post.data.description}
-                      />
-                    ))}
-                  </div>
+                <header className='max-w-2xl'>
+                  <p className=' text-md uppercase text-[#DA3028] font-display font-semibold'>blogs</p>
+                  <h1 className='text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100'>
+                  Exploring modern trends and news in the technology sector through informative blogs
+                  </h1>
+                  <p className='mt-6 text-base text-zinc-600 dark:text-zinc-400'>
+                     All of my long-form thoughts on technology, programming, AI, and more, collected in chronological order.
+                  </p>
+                </header>
+                <div className='mt-16 sm:mt-20'>
+                    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 w-full h-auto'>
+                      {posts.map((post, index) => (
+                          <BlogCards
+                            key={index}
+                            title={post.data.title}
+                            time={post.data.time}
+                            description={post.data.description}
+                          />
+                        ))}   
+                    </div>
                 </div>
               </div>
             </div>
@@ -58,3 +57,4 @@ export function getStaticProps() {
 
     return { props : {posts}}
 }
+
---
+++ portfolio/pages/components/BlogCards.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import Image from "next/image";
+import BlogsImage from '../../public/INT0.5.jpg'
+import profil from "../../public/PRpic.png";
+
+const BlogCards = ({ title, description, time}) => {
+  return (
+    <>
+        <div class="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-zinc-800 dark:border-gray-700 flex flex-col transition duration-300 hover:scale-105">
+            <div className="flex justify-between">
+                <div className="flex px-5 py-4">
+                    <div>
+                        <Image
+                        className="rounded-full max-w-none"
+                        src={profil}
+                        alt=""
+                        width={35}
+                        height={35}
+                    />
+                    </div>
+                    <div className="flex flex-col ml-5 text-xs">
+                        <div className="inline-block font-bold dark:text-white">Tony Bui</div>
+                        <div className="text-slate-500 dark:text-slate-300 dark:text-slate-400">{time}</div>
+                    </div>
+                </div>
+            </div>
+            <div class="flex justify-center">
+                <Image
+                    class="rounded-lg"
+                    src={BlogsImage}
+                    alt=""
+                    width={275}
+                    height={200}
+                />
+            </div>
+            <h5 class="mt-2 text-lg px-6 py-2 font-bold tracking-tight text-gray-900 dark:text-white">
+                {title}
+            </h5>
+            <hr class="h-px mx-auto my-2 w-3/4 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+            <div class="px-5">
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                    {description}
+                </p>    
+                <div aria-hidden="true" className="relative mt-4 mb-4 flex items-center text-sm font-medium text-sky-300 underline hover:cursor-pointer">Read full blog</div>    
+            </div>
+            
+        </div>
+    </>
+  );
+};
+
+export default BlogCards;
